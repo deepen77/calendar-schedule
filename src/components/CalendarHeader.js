25edfa7af5dec3383c79a6687ca9a13473b28cf5
deepen77 <przemek.starostka@yahoo.com>
2022-04-01 21:03:16 +0200

    calendar header added

--- src/components/CalendarHeader.js
+++ src/components/CalendarHeader.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import logo from '../assets/calendar-icon.png'
+import {HiChevronLeft, HiChevronRight } from "react-icons/hi";
+import './CalendarHeader.css'
+
 
 const CalendarHeader = () => {
   return (
-    <div>CalendarHeader</div>
-  )
+    <header className="CalendarHeader__wrapper">
+      <img className="CalendarHeader__logo" src={logo} alt="logo of calendar" />
+      <h1 className="CalendarHeader__title">Calendar</h1>
+      <button className="CalendarHeader__btn-today">Today</button>
+      <button className="CalendarHeader__btn-left">
+        <HiChevronLeft />
+      </button>
+      <button className="CalendarHeader__btn-right">
+        <HiChevronRight />
+      </button>
+    </header>
+  );
 }
 
 export default CalendarHeader
